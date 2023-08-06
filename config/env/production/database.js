f3497238fbace2bad43b86db20079bd4eb147090
Kyrylo Orlov <dev@kyryloorlov.com>
2023-08-05 21:57:33 -0400

    Revert back to droplet

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,16 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("APP_URL"), // Sets the public URL of the application.
-  app: {
-    keys: env.array("APP_KEYS"),
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapidb"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+      ssl: env.bool("DATABASE_SSL", false) && {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
+    },
+    debug: false,
   },
 });
